d08740aff1a7ffda897d8d57adea1b968f03a381
YolandaVanesa <159740391+YolandaVanesa@users.noreply.github.com>
2024-07-16 19:11:06 -0400

    Add files via upload

---
+++ AluraForo/src/main/java/com/proyectoForoAluraCursosYolandaCanete/AluraForo/AluraForoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoForoAluraCursosYolandaCanete.AluraForo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraForoApplication.class, args);
+	}
+
+}
